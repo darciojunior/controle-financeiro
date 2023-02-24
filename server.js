0c10c3b9e0ad1af1a5f46033aaa67e6826a94f7f
Darcio Junior <darciojunior@terra.com.br>
2023-02-24 11:09:31 -0300

    feat: Add Routes and Controllers Structure for User

--- server.js
+++ server.js
@@ -5,13 +5,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import connectDB from "./db/connect.js";
+
+//routers
+import authRouter from "./routes/authRoutes.js";
+
 //middleware
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
+
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
